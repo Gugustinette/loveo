2ee075736959b51ba6f4441faa0bba62a9e763a2
Gugustinette <mercier.augustin@outlook.fr>
2025-02-12 15:43:19 +0100

    feat: config shadcn-vue

---
+++ components/GenderSelect.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+</script>
+
+<template>
+  <Select>
+    <SelectTrigger>
+      <SelectValue placeholder="Je cherche ..." />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectItem value="men">
+          Un homme
+        </SelectItem>
+        <SelectItem value="women">
+          Une femme
+        </SelectItem>
+        <SelectItem value="anything">
+          Peu importe
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</template>
