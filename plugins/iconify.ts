287002a6cbca61b213dab19d90f88f3c6236c09a
Gugustinette <mercier.augustin@outlook.fr>
2025-02-12 18:43:16 +0100

    ci: init github ci release

--- plugins/iconify.ts
+++
@@ -1,7 +0,0 @@
-// plugins/iconify.ts
-import { defineNuxtPlugin } from '#app';
-import { Icon } from '@iconify/vue';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('Icon', Icon);
-});
